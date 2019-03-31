ca459f00f3ece1a3a8516ab4ce591816e520ad47
Oskar Rajzer <s15342@pjwstk.edu.pl>
2019-03-31 18:57:14 +0100

    Commit z laptopa

--- Assets/Scripts/Alg/Algorytm.cs
+++ Assets/Scripts/Alg/Algorytm.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,9 +10,13 @@ public class Algorytm : MonoBehaviour
 	public readonly static float MEDIUM_ARM_LENGTH = 12.282f+0.5f; // +korekta jointa
 	public readonly static float UPPER_ARM_LENGTH = 14.5079f+0.25f+(0.4835968f);// +korekta jointa i chwytaka
 
+    public readonly static float MAX_LENGTH = LOWER_ARM_LENGTH+MEDIUM_ARM_LENGTH+UPPER_ARM_LENGTH;
+
         // Debug.Log(joint2.transform.position.y - joint1.transform.position.y);
         // Debug.Log(joint3.transform.position.y - joint2.transform.position.y);
         // Debug.Log(chwytak.transform.position.y - joint3.transform.position.y);
+
+    // public static 
 
     public MessageShower ms;
 
@@ -33,6 +38,9 @@ public class Algorytm : MonoBehaviour
     float[] currentArmAngles = new float[]{90, 90, 90};
     float[] moveDirection = new float[]{1, 1, 1};
 
+    // private bool isReady = false;
+    // public static int canChangeObjekt = 0b0_0001;
+
     void Start()
     {
         ms = MessageShower.instanceOf(GameObject.Find("Warning"));
@@ -49,45 +57,60 @@ public class Algorytm : MonoBehaviour
 
         try{
             calculateAllData();
-        }catch(AngleValueException ave){ 
+        }catch(Exception ave){ 
             Debug.Log(ave.Message);
-            armAngles = new float[]{1000f, 1000f, 1000f};
-            // ms.visible = true;
+            // armAngles = new float[]{1000f, 1000f, 1000f};
             ms.text = ave.Message;
+            // isReady = false;
         }
     }
 
     void Update()
     {
+        // if(isReady){
         float speed = 0.25f;
         if(startAtZero <  targetAngle){
             joint1.transform.RotateAround(joint1.transform.position, new Vector3(0, 1, 0), rotateDirection*speed);
             startAtZero+=speed;
+        }else{
+            // canChangeObjekt = canChangeObjekt << 1;
         }
 
         if(currentArmAngles[0] > armAngles[0]){
             dolne.transform.RotateAround(joint1.transform.position, joint1.transform.right, moveDirection[0]*speed);
             currentArmAngles[0]-=speed;
+        }else{
+            // canChangeObjekt = canChangeObjekt << 1;
         }
         if(currentArmAngles[1] > armAngles[1]){
-
             srodkowe.transform.RotateAround(joint2.transform.position, joint2.transform.right, moveDirection[1]*speed);
             currentArmAngles[1]-=speed;
+        }else{
+            // canChangeObjekt = canChangeObjekt << 1;
         }
         if(currentArmAngles[2] > armAngles[2]){
             gorne.transform.RotateAround(joint3.transform.position, joint3.transform.right, moveDirection[2]*speed);
             currentArmAngles[2]-=speed;
+        }else{
+            // canChangeObjekt = canChangeObjekt << 1;
         }
-
 
         if(Input.GetKeyDown(KeyCode.B)){
             try{
                 calculateAllData();
                 startAtZero = 0;
-            }catch(AngleValueException ave) { Debug.Log(ave.Message);   Debug.Log("Blad"); ms.text = ave.Message; }
+                // canChangeObjekt = 0b0_0001;
+            }catch(Exception ave) {
+                Debug.Log(ave.Message);
+                ms.text = ave.Message;
+                // isReady = false;
+            }
         }
+        // }
+        // if(canChangeObjekt == 0b1_0000){
+        //     isReady = false;
 
-
+        // }
     }
 
     public float rotateTowardsTarget(Vector3 target)
@@ -119,16 +142,16 @@ public class Algorytm : MonoBehaviour
     public float[] moveTowardsTarget_Perpendicular(Vector3 target){
 
         float angleCorrection = 0;
-        // if(target.y > obiekt.GetComponent<Renderer>().bounds.size.y/2){
-        //     angleCorrection = Mathf.Asin( (target.y-joint1.transform.position.y)/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
-        // }else{
-        //     angleCorrection = -Mathf.Acos( Mathf.Sqrt( Mathf.Pow(target.x,2)+Mathf.Pow(target.z,2))/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
-        // }
-        if(target.y < joint1.transform.position.y){
-            angleCorrection =  Mathf.Asin( (target.y-joint1.transform.position.y)/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
+        if(target.y > obiekt.GetComponent<Renderer>().bounds.size.y/2){
+            angleCorrection = Mathf.Asin( (target.y-joint1.transform.position.y)/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
         }else{
-            angleCorrection = Mathf.Asin( target.y/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
+            angleCorrection = -Mathf.Acos( Mathf.Sqrt( Mathf.Pow(target.x,2)+Mathf.Pow(target.z,2))/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
         }
+        // if(target.y < joint1.transform.position.y){
+        //     angleCorrection =  Mathf.Asin( (target.y-joint1.transform.position.y)/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
+        // }else{
+        //     angleCorrection = Mathf.Asin( target.y/Vector3.Distance(target, joint1.transform.position) )*Mathf.Rad2Deg;
+        // }
 
 
         float distance = Vector3.Distance(target, joint1.transform.position);
@@ -146,6 +169,13 @@ public class Algorytm : MonoBehaviour
 
 
     public void calculateAllData(){
+
+        if(Vector3.Distance(obiekt.transform.position, joint1.transform.position) > MAX_LENGTH){
+            Debug.Log(Vector3.Distance(obiekt.transform.position, joint1.transform.position));
+            Debug.Log(MAX_LENGTH);
+            throw new Exception("Target too far");
+        }
+
             float angle = rotateTowardsTarget(obiekt.transform.position);
 
             if(joint1.transform.forward.x*obiekt.transform.position.x < 0){
@@ -163,6 +193,9 @@ public class Algorytm : MonoBehaviour
             for(int i = 0; i<armAngles.Length; i++)
                 if(armAngles[i] > 90)
                     throw new AngleValueException("Arm "+i+" has an angle greater than 90");
+                else if(Double.IsNaN(armAngles[i]))
+                    throw new FormatException("Number is NaN");
+                
 
             for(int i = 0; i<armAngles.Length; i++){
                 if(currentArmAngles[i] < armAngles[i]){
@@ -171,7 +204,8 @@ public class Algorytm : MonoBehaviour
                 }else{
                     moveDirection[i] = 1;
                 }
-            }
+            // isReady = true;
+        }
 
 
     }
--- Assets/Scripts/TargetMoving.cs
+++ Assets/Scripts/TargetMoving.cs
@@ -1,40 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Algorytm;
 
 public class TargetMoving : MonoBehaviour
 {
 
     public GameObject obiekt;
     float movementSpeed = 40f;
-    float length;
+    // float length;
 
     void Start()
     {
         obiekt = GameObject.Find("Obiekt");
-        length = GameObject.Find("Chwytak").transform.position.y;
+        // length = GameObject.Find("Chwytak").transform.position.y;
     }
 
     void Update()
     {
+        // if(canChangeObjekt == 0b1_0000){
         if(Input.GetKey(KeyCode.UpArrow)){
-            if((obiekt.transform.position + Vector3.forward*Time.deltaTime*movementSpeed).magnitude <= length)
+            if((obiekt.transform.position + Vector3.forward*Time.deltaTime*movementSpeed).magnitude <= MAX_LENGTH)
                 obiekt.transform.position += Vector3.forward*Time.deltaTime*movementSpeed;
         }
         if(Input.GetKey(KeyCode.DownArrow)){
-            if((obiekt.transform.position + -1*Vector3.forward*Time.deltaTime*movementSpeed).magnitude <= length)
+            if((obiekt.transform.position + -1*Vector3.forward*Time.deltaTime*movementSpeed).magnitude <= MAX_LENGTH)
                 obiekt.transform.position += -1*Vector3.forward*Time.deltaTime*movementSpeed;
         }
         if(Input.GetKey(KeyCode.RightArrow)){
-            if(( obiekt.transform.position + Vector3.right*Time.deltaTime*movementSpeed).magnitude <= length)
+            if(( obiekt.transform.position + Vector3.right*Time.deltaTime*movementSpeed).magnitude <= MAX_LENGTH)
                 obiekt.transform.position += Vector3.right*Time.deltaTime*movementSpeed;
         }
         if(Input.GetKey(KeyCode.LeftArrow)){
-            if((obiekt.transform.position + -1*Vector3.right*Time.deltaTime*movementSpeed).magnitude <= length)
+            if((obiekt.transform.position + -1*Vector3.right*Time.deltaTime*movementSpeed).magnitude <= MAX_LENGTH)
                 obiekt.transform.position += -1*Vector3.right*Time.deltaTime*movementSpeed;
         }
         if(Input.GetKey(KeyCode.Space)){
-            if((obiekt.transform.position + Vector3.up*Time.deltaTime*movementSpeed).magnitude <= length)
+            if((obiekt.transform.position + Vector3.up*Time.deltaTime*movementSpeed).magnitude <= MAX_LENGTH)
                 obiekt.transform.position += Vector3.up*Time.deltaTime*movementSpeed;
         }
         if(Input.GetKey(KeyCode.LeftControl)){
@@ -44,5 +46,6 @@ public class TargetMoving : MonoBehaviour
                 obiekt.transform.position = new Vector3(obiekt.transform.position.x, 0+obiekt.GetComponent<Renderer>().bounds.size.y/2, obiekt.transform.position.z);
             }
         }
+        // }
     }
 }
